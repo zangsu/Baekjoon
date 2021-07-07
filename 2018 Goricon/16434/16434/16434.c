5dc7daf36c4102deee589a8a3027f3f015697d41
zangsu <76612738+zangsu@users.noreply.github.com>
2021-07-07 17:18:43 +0900

    Add files via upload

--- 2018 Goricon/16434/16434/16434.c
+++ 2018 Goricon/16434/16434/16434.c
@@ -6,12 +6,16 @@ typedef struct room{
 	int t;
 	int a;
 	int h;
+	int roomMax;
+	int Max;
+	int heal;
 }room;
 typedef struct man {
 	int atk;
 	int maxhp;
 	int nowhp;
 }man;
+int Rhp(int, int, int);
 int main()
 {
 	int n, Hatk, i;
@@ -19,7 +23,34 @@ int main()
 	link rooms = NULL;
 	rooms = (link)malloc(sizeof(room) * n);
 	scanf("%d %d", &n, &hero.atk);
-
+	for (i = 0; i < n; i++)
+	{
+		scanf("%d %d %d", &rooms[i].t, &rooms[i].a, &rooms[i].h);
+		if (rooms[i].t == 1)
+		{
+			rooms[i].roomMax = Rhp(rooms[i].h, rooms[i].a, hero.atk);
+			if (!(i - 1))
+				rooms[i].Max = rooms[i].roomMax;
+			else
+			{
+				if (rooms[i - 1].t == 1)
+				{
+					rooms[i].Max = rooms[i].roomMax + rooms[i - 1].Max;
+				}
+			}
+		}
+		else
+		{
+			hero.atk += rooms[i].a;
+		}
+	}
 
 	free(rooms);
+}
+int Rhp(int Mhp, int Matk, int Hatk)
+{
+	if (Mhp % Hatk == 0)
+		return (((Mhp / Hatk) - 1) * Matk) + 1;
+	else
+		return ((Mhp / Hatk) * Matk) + 1;
 }
