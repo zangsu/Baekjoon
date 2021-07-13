0682fbe0428375f1ddb69e0474e6464481c91318
zangsu <76612738+zangsu@users.noreply.github.com>
2021-07-13 15:58:09 +0900

    Add files via upload

--- 2018 Goricon/16432/16432/16432.c
+++ 2018 Goricon/16432/16432/16432.c
@@ -4,7 +4,7 @@
 int* findans(int, int**);
 int main()
 {
-	int N, i, j;
+	int N, i, j, det = 0;
 	int** a = NULL;
 	int* ans = NULL;
 	scanf("%d", &N);
@@ -19,49 +19,64 @@ int main()
 		for (j = 0; j < temp; j++)
 			scanf("%d", &a[i][j+1]);
 	}
-	ans = findans(N, a);
+	//ans = findans(N, a);
+	for (i = 0; i < N; i++)
+		ans[i] = 0;
+	while (det == 0)
+	{
+		int check = 1;
+		for (i = 0; i < N-1; i++)
+		{
+			for (j = i + 1; j < N; j++)
+			{
+				if (a[i][ans[i]] == a[j][ans[j]])
+				{
+					check = 0;
+					break;
+				}
+			}
+			if (check == 0)
+				break;
+		}
+		if (check == 1)
+			det = 1;
+	}
 	free(a);
-	if (ans[0] == -1)
+	if(det == 0)
 		printf("-1");
 	else
 		for (i = 0; i < N; i++)
 			printf("%d\n", a[i][ans[i]]);
 
 }
-int* findans(int N, int** a)
-{
-	int i, j, k;
-	int* ans = NULL;
-	ans = malloc(sizeof(int) * N);
-	for (i = 0; i < N; i++)
-		ans[i] = 0;
-	while (1)
-	{
-		/*
-		ans에는 각 줄의 몇번째 떡인지 저장
-ans에 저장하기
-
-000이 안되면
-001
-002
-010
-011
-012
-100
-101
-102
-
-
-012->100으로 넘어가는거
-2가 더 넘길 숫자 없으니 2는 다시 0, 
-1이 더 넘길 숫자 없으니 1은 다시 0. 
-0을 1로.*/
-		for (i = 0; i < N; i++)
-		{
-			for(j = i; j<N; j++)
-				if(a[])
-		}
-	}
-	ans[0] = -1;
-	return ans;
-}
+//int* findans(int N, int** a)
+//{
+//	int i, j, k;
+//	int* ans = NULL;
+//	ans = malloc(sizeof(int) * N);
+//	for (i = 0; i < N; i++)
+//		ans[i] = 0;//i-1번째 원소가 i번째 날의 떡 위치
+//	while()
+//	//while (1)
+//	//{
+//	//	for (i = 0; i < N; i++)
+//	//	{
+//	//		int det = 0;
+//	//		for (j = i + 1; j < N; j++)
+//	//		{
+//	//			if (a[i][ans[i]] == a[j][ans[j]])
+//	//			{
+//	//				det = 1;
+//	//				break;
+//	//			}
+//	//		}
+//	//		if (det == 0)
+//	//		{
+//	//			//한자리씩 올려야하는데 for문 써야할듯
+//	//		}
+//	//	}
+//	//}
+//
+//	ans[0] = -1;
+//	return ans;
+//}
