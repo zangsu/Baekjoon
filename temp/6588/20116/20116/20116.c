c78382cbc074574a96c8c42ea619a74cdbee89f4
zangsu <76612738+zangsu@users.noreply.github.com>
2021-07-27 01:23:45 +0900

    Add files via upload

---
+++ temp/6588/20116/20116/20116.c
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	int n, i = 0;
+	float sum = 0, l;
+	int* box = NULL;
+	scanf("%d %f", &n, &l);
+	box = (int*)malloc(sizeof(int) * n);
+	for (i = 0; i < n; i++)
+		scanf("%d", &box[i]);
+	for (i = n - 1; i > 0; i--)
+	{
+		sum += box[i];
+		if (sum / (n - i) >= box[i - 1] + l || sum / (n - i) <= box[i - 1] - l)
+			break;
+	}
+	if (i == 0)
+		printf("stable");
+	else
+		printf("unstable");
+	free(box);
+}
