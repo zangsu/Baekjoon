3d7c873772cd29a12bea27be0e5c51b994ac57cd
zangsu <76612738+zangsu@users.noreply.github.com>
2021-08-08 02:30:59 +0900

    Add files via upload

--- BOJ 길라잡이/1920/1920/1920/1920.c
+++ BOJ 길라잡이/1920/1920/1920/1920.c
@@ -1,7 +1,36 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include<stdio.h>
-#include<stdlib.h>
-int main() //1920¹ø
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()//Á¤·Ä + Å½»ö < ÀüÃ¼ Å½»ö?
 {
-	int i, a;
+	int n, m, i, j;
+	int* a = NULL;
+	int* list = NULL;
+
+	scanf("%d", &n);
+	a = (int*)malloc(sizeof(int) * n);
+	for (i = 0; i < n; i++)
+		scanf("%d", &a[i]);
+	scanf("%d", &m);
+	list = (int*)malloc(sizeof(int) * m);
+	for (i = 0; i < m; i++)
+		scanf("%d", &list[i]);
+
+	for (i = 0; i < m; i++)
+	{
+		for (j = 0; j < n; j++)
+		{
+			if (list[i] == a[j])
+			{
+				printf("1\n");
+				break;
+			}
+		}
+		if (j == n)
+			printf("0\n");
+	}
+
+	free(a);
+	free(list);
 }
