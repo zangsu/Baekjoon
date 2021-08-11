7a1569715a3cd6d7fbea6ffca63ee5d2c1d55934
zangsu <76612738+zangsu@users.noreply.github.com>
2021-08-11 10:20:19 +0900

    Add files via upload

--- BOJ 길라잡이/1920/1920/1920.c
+++ BOJ 길라잡이/1920/1920/1920.c
@@ -1,7 +1,72 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include<stdio.h>
-#include<stdlib.h>
-int main()
-{
+#include <stdio.h>
+#include <stdlib.h>
+int* sort(int*, int, int);
+void temp();
 
+int main()//Á¤·Ä + Å½»ö < ÀüÃ¼ Å½»ö?
+{
+	temp();
+	// int n, m, i, j;
+	// int * a = NULL;
+	// int * list = NULL;
+
+	// scanf("%d", &n);
+	// a = (int *)malloc(sizeof(int) * n);
+	// for(i = 0; i<n; i++)
+	// 	scanf("%d", &a[i]);
+	// scanf("%d", &m);
+	// list = (int *)malloc(sizeof(int)*m);
+	// for(i = 0; i<m; i++)
+	// 	scanf("%d", &list[i]);
+
+	// a = sort(a, 0, n);
+
+
+	// free(a);
+	// free(list);
+}
+// int * sort(int * a, int front, int end)
+// {
+// 	int pivot = front;
+// 	int head = front+1, tail = end;
+// 	while(head < tail)
+// 	{
+// 		while(a[head] < a[pivot])
+// 			head++;
+// 		while(a[tail] > a[pivot])
+// 			tail--;
+// 		int temp = a[head];
+// 		a[head] = a[tail];
+// 		a[tail] = temp;
+// 	}
+// 	int temp = a[pivot];
+// 	a[tail] = a[pivot];
+// 	a[pivot] = temp;
+
+// 	if(front ~~)
+// 		a = sort(a);
+// 	if(end ~~)
+// 		a = sort(a);
+
+// 	return a;
+
+// }
+
+void temp()
+{
+	int n, i, temp, a[100000] = { 0 };
+	int* list = NULL;
+	scanf("%d", &n);
+	for (i = 0; i < n; i++)
+	{
+		scanf("%d", &temp);
+		a[temp] = 1;
+	}
+	scanf("%d", &n);
+	list = (int*)malloc(sizeof(int) * n);
+	for (i = 0; i < n; i++)
+		scanf("%d", &list[i]);
+	for (i = 0; i < n; i++)
+		printf("%d", a[list[i]]);
 }
