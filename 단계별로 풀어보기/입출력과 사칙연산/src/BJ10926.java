1a00729e5f156bac0c5c04bce0d5e6472e8bc78d
zangsu <zangsu_@naver.com>
2022-04-06 19:31:13 +0900

    입출력과 사칙연산
    Goorm.ide에 있던 코드 push

---
+++ 단계별로 풀어보기/입출력과 사칙연산/src/BJ10926.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class BJ10926{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String Id = sc.nextLine();
+        System.out.printf("%s??!", Id);
+        sc.close();
+    }
+}
---
+++ 단계별로 풀어보기/입출력과 사칙연산/src/BJ18108.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class BJ18108{
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+        int year = sc.nextInt();
+        System.out.printf("%d", year-543);
+    }
+}
